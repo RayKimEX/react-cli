947ae51a9773ab15bd30ec6f3d38c4d1acc697c5
Ray Kim <ray.kim@mojostudio.io>
2019-08-28 17:10:14 +0900

    added generate file feature

--- function/util.sh
+++ function/util.sh
@@ -4,3 +4,11 @@ strindex() {
 	x="${1%%$2*}"
 	[[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
 }
+
+capitalize_remove_punctuation()
+{
+  local words IFS=$' \t\n-\'.,;!:*?' #Handle hyphenated names and punctuation
+  read -ra words <<< "${@,,}"
+  IFS=''
+  echo "${words[*]^}"
+}
